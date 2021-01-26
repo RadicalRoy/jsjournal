90e24c65629e8b0310f667836d5d33eeea5f9dd1
MathematicalYogi <rolandquintana1991@gmail.com>
2021-01-25 18:41:44 -0800

    add ResizableBox library for better user experience. debounce bundling.

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,8 +1,11 @@
+import './preview.css';
 import { useEffect, useRef } from 'react';
 
 const html = `
 <html>
-<head></head>
+<head>
+  <style>html { background-color: white; } </style>
+</head>
 <body>
   <div id="root"></div>
   <script>
@@ -29,16 +32,21 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
 
 	useEffect(() => {
 		iframe.current.srcdoc = html;
-		iframe.current.contentWindow.postMessage(code, '*');
+		// ensure srcdoc has updated with html before posting
+		setTimeout(() => {
+			iframe.current.contentWindow.postMessage(code, '*');
+		}, 50);
 	}, [code]);
 
 	return (
-		<iframe
-			ref={iframe}
-			title="preview"
-			srcDoc={html}
-			sandbox="allow-scripts"
-		/>
+		<div className="preview-wrapper">
+			<iframe
+				ref={iframe}
+				title="preview"
+				srcDoc={html}
+				sandbox="allow-scripts"
+			/>
+		</div>
 	);
 };
 
